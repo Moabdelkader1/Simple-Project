a97909e5e06efac726d55ac816abdda0513d3219
Moabdelkader1 <moabdelkader123@gmail.com>
2024-08-27 20:50:42 +0300

    adding calculator feature

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hello sic community")
+
+def calc(n1,n2,op):
+    if op == "+":
+        print(f"{n1} + {n2} = {n1+n2}")
+    elif op == "-":
+        print(f"{n1} - {n2} = {n1-n2}")
+    elif op == "*":
+        print(f"{n1} * {n2} = {n1*n2}")
+    elif op == "/":
+        print(f"{n1} / {n2} = {n1/n2}")
+    else:
+        print("Invalid operation")
+    
